85649a77e3256cf6a8b989711f6a63a6b7d1ccd0
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-06-26 23:29:15 -0600

    Update

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
 
-// Connect to MongoDB
-// Modify the URI to connect to the database in Docker
-// const mongooseURIDocker = 'mongodb://host.docker.internal:27017/dbProducts';
-// Modify the URI to connect to the database in localhost
+// Conexión a MongoDB
 const mongooseURI = 'mongodb+srv://bryan_quezada:AfOYD6Ju7f70PSA7@cluster0.1yrzoce.mongodb.net/APIReportes?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect((mongooseURI), {
-}).then(() => {
-    console.log('Connected to MongoDB');
+mongoose.connect(mongooseURI)
+.then(() => {
+  console.log('Connected to MongoDB');
 }).catch((error) => {
-    console.log('Error connecting to MongoDB', error);
+  console.error('Error connecting to MongoDB:', error.message);
 });
 
-// Routes
+// Rutas
 const productsRouter = require('./routers/products'); 
 app.use('/api/products', productsRouter);
 
-app.use(express.static('public'));
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-// Start server
+// Iniciar servidor
 app.listen(port, () => {
-    console.log(`Server listen in http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
